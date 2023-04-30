3fc06f44c29d0659b250474e2b3e6f354811cbb0
Ogierumua <ogierumuaanthony@gmail.com>
2023-04-30 14:24:51 -0600

    CourseJson file structure was changed, becuase i have issues implimenting the previous file structure using javacript

---
+++ webserver/src/course.js
@@ -0,0 +1,56 @@
+fetch("course.json")
+    .then(function(response) {
+        return response.json();
+    })
+
+    .then(function(courses) {
+        let placeholder = document.querySelector("#data-output");
+        let out = "";
+        for (let course of courses) {
+            out += `
+        <tr>
+        <td> <img src = "${course.Images}"> </td>
+        <td> ${course.SubjectArea} </td>
+        <td> ${course.CourseName} </td>
+        <td> ${course.CourseDetails} </td>
+        <td> ${course.Level} </td>
+        <td> ${course.EntryRequirement} </td>
+        <td> ${course.FeesandFunding} </td>
+        <td> ${course.KeyFacts} </td>
+        </tr>
+        `;
+        }
+        placeholder.innerHTML = out;
+    })
+
+
+ //setInterval(function() {
+//     $.ajax({
+//         url: 'course.json',
+//         dataType: 'json',
+//         success: function(data) {
+//             for (var i=0; i < data.length; i++) {
+//                 var row = $(
+//                     '<tr><td>' 
+//                      + data[i].SubjectArea + 
+//                     '</td><td>'
+//                          + data[i].CourseName + 
+//                     '</td><td>'
+//                         + data[i].CourseType + 
+//                     '</td><td>' 
+//                         + data[i].CourseDetails +
+//                     '</td><td>'
+//                         + data[i].EntryRequirement +
+//                     '</td><td>'
+//                         + data[i].FeesandFunding +
+//                     '</td><td>'
+//                         + data[i].KeyFacts + 
+//                     '</td></tr>');
+//                 $('#data-output').append(row);
+//             }
+//         },
+//         error: function(textStatus, errorThrown){
+//             alert('Error: ' + textStatus + ' - ' + errorThrown);
+//         }
+//     });
+// }, 10000);
